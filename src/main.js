f76f66dcb017aa1ad6151e9cad04d39ff1d4550c
unknown <shyndard@gmail.com>
2019-10-22 16:38:49 +0200

    :sparkles: Add pagination to profile

--- src/main.js
+++ src/main.js
@@ -12,10 +12,45 @@ library.add(faHeart)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
+const Keycloak = require('keycloak-js');
+const fs = require('fs');
+
 Vue.config.productionTip = false
 
-new Vue({
-  el: "#app",
-  router: router,
-  render: h => h(App)
-})
+let initOptions = JSON.parse(fs.readFileSync('../keycloak.json'));
+let keycloak = Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) =>{
+    
+    if(!auth) {
+      window.location.reload();
+    } else {
+      Vue.$log.info("Authenticated");
+    }
+ 
+    new Vue({
+      render: h => h(App),
+    }).$mount('#app')
+  
+
+    localStorage.setItem("vue-token", keycloak.token);
+    localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
+
+    setTimeout(() =>{
+      keycloak.updateToken(70).success((refreshed)=>{
+        if (refreshed) {
+          Vue.$log.debug('Token refreshed'+ refreshed);
+        } else {
+          Vue.$log.warn('Token not refreshed, valid for '
+          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+        }
+      }).error(()=>{
+          Vue.$log.error('Failed to refresh token');
+      });
+
+
+    }, 60000)
+
+}).error(() =>{
+  Vue.$log.error("Authenticated Failed");
+});
