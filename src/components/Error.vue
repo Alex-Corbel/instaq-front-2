bb162dc1cbd21350f62ed5bd548484bf95e90e16
unknown <shyndard@gmail.com>
2019-10-22 09:45:47 +0200

    :lipstick: Add css to 404 page

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="has-text-centered ">
-    <h1 class="title is-1">Error 404 : Page not found !!</h1>
+  <div class="notfound">
+      <h2>404 - Page introuvable</h2>
+      <p>La page que vous avez demandé n'existe pas ou a été supprimée.</p>
+      <router-link to="/"><button class="button is-primary">Direction l'accueil</button></router-link>
   </div>
 </template>
 
-<script>
-export default {
-  name: "Error",
-  props: {
-    msg: String
-  }
-};
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .notfound {
+    padding-top: 100px;
+    text-align: center;
+  }
+  .notfound>h2 {
+    font-size: 50px;
+    padding-bottom: 20px;
+  }
 </style>
