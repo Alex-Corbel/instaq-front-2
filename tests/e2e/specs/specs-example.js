8f2457f284cf98d3cd2bd7390258039fa23d53dc
Louis Perdereau <louis.perdereau.epsi@gmail.com>
2019-11-12 10:53:59 +0100

    Issue #15: Init test architecture

---
+++ tests/e2e/specs/specs-example.js
@@ -0,0 +1,60 @@
+// Name file example.spec.js
+
+/*var url = 'http://localhost:8080/#/'
+
+describe('Burger Menu Testing', () => {
+  beforeEach(() => {
+    cy.visit(url)
+  })
+
+  it('isBurgerMenuVisible', () => {
+    var burgerMenu = cy.get('[class="bm-burger-button"]')
+    burgerMenu.should('be.visible')
+  })
+
+  it('isBurgerMenuButtonsAreVisible', () => {
+    var burgerMenu = cy.get('[class="bm-burger-button"]')
+    burgerMenu.click()
+    var calculatorSpan = cy.get('[href="#/calculator"]').find('span').contains('Calculatrice')
+    var newPharmaSpan = cy.get('[href="#/newstore"]').find('span').contains('Nouvelle Pharmacie')
+    var carteSpan = cy.get('[href="#/"]').find('span').contains('Carte')
+    carteSpan.should('be.visible')
+    newPharmaSpan.should('be.visible')
+    calculatorSpan.should('be.visible')
+  })
+
+  it('isCalculatorAvailableFromBurgerMenu', () => {
+    var burgerMenu = cy.get('[class="bm-burger-button"]')
+    burgerMenu.click()
+    cy.get('[href="#/calculator"]').click()
+
+    var calculatorView = cy.get('[class="calculator"]')
+    calculatorView.should('be.visible')
+  })
+
+  it('isNewStoreAvailableFromBurgerMenu', () => {
+    var burgerMenu = cy.get('[class="bm-burger-button"]')
+    burgerMenu.click()
+    cy.get('[href="#/newstore"]').click()
+
+    var newStoreView = cy.get('[class="newstore"]')
+    newStoreView.should('be.visible')
+  })
+})
+
+describe('Map testing', () => {
+  beforeEach(() => {
+    cy.visit(url)
+  })
+
+  it('isInfoWindowVisible', () => {
+    var infoWindow = cy.get('[class="infowindow"]')
+    infoWindow.should('not.be.visible')
+  })
+
+  it('isRedCircleVisible', () => {
+    var infoWindow = cy.get('[class="infowindow"]')
+    infoWindow.should('not.be.visible')
+  })
+})
+*/
---
+++ tests/unit/specs/spec-example.js
@@ -0,0 +1,22 @@
+// Name file example.spec.js
+
+/*import InfoWindowMobile from '@/components/InfoWindowMobile'
+import { mount } from '@vue/test-utils'
+
+describe('InfoWindowMobile.vue default', () => {
+  it('Check default variable', () => {
+    expect(typeof InfoWindowMobile.data).toBe('function')
+    const defaultData = InfoWindowMobile.data()
+    expect(defaultData.infowindow).toBe(true)
+    expect(defaultData.isHidden).toBe(true)
+  })
+})
+
+describe('showHideInfo function', () => {
+  const wrapper = mount(InfoWindowMobile)
+  it('Test showHideInfo', () => {
+    let display = wrapper.vm.isHidden
+    wrapper.vm.showHideInfos()
+    expect(wrapper.vm.isHidden).toBe(!display)
+  })
+})*/
